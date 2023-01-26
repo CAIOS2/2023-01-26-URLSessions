8db54be3f3071473bbe73d251054c41e20e6f4c2
ParadmiGit <103635348+ParadmiGit@users.noreply.github.com>
2023-01-26 19:40:01 +0200

    done

--- urlsessions/Services/PlanetsAPI.swift
+++ urlsessions/Services/PlanetsAPI.swift
@@ -9,27 +9,27 @@ import Foundation
 
 enum Constants: String {
     case baseURL = "http://swapi.dev/api/"
-    case planetsEndpoint = "planets/1/"
-
-    static func getURL(for constant: Constants) -> URL {
-        var urlString = baseURL.rawValue + constant.rawValue
+    case planetsEndpoint = "planets/"
+    
+    static func getURL(for constant: Constants, id: Int) -> URL {
+        var urlString = baseURL.rawValue + constant.rawValue + String(id) + "/"
         return URL(string: urlString)!
     }
 }
 
-
 class PlanetsAPI {
-
+    
     enum APIError: Error {
         case parsingFailed
         case fetchFailed
     }
-
+    
     private let decoder = JSONDecoder()
     private(set) var task: URLSessionDataTask?
-
-    func fetchPlanets(completion: @escaping (Result<Planet, APIError>) -> Void) {
-        performRequest(url: Constants.getURL(for: .planetsEndpoint)) { [weak self] result in
+    
+    func fetchPlanets(id: Int, completion: @escaping (Result<Planet, APIError>) -> Void) {
+        
+        performRequest(url: Constants.getURL(for: .planetsEndpoint, id: id), callback: { [weak self] result in
             guard let self else { return }
             switch result {
             case .success(let data):
@@ -42,20 +42,20 @@ class PlanetsAPI {
             case .failure(let failure):
                 completion(.failure(failure))
             }
-        }
-    }
-
-    private func performRequest(url: URL?, callback: @escaping (Result<Data, APIError>) -> Void) {
-        guard let url else { return }
-
-        let session = URLSession(configuration: .default)
-        task = session.dataTask(with: url) { data, response, error in
-            if let data {
-                callback(.success(data))
-            } else {
-                callback(.failure(.fetchFailed))
-            }
-        }
-        task?.resume()
-    }
+        })
 }
+                       
+                       private func performRequest(url: URL?,  callback: @escaping (Result<Data, APIError>) -> Void) {
+            guard let url else { return }
+            
+            let session = URLSession(configuration: .default)
+            task = session.dataTask(with: url) { data, response, error in
+                if let data {
+                    callback(.success(data))
+                } else {
+                    callback(.failure(.fetchFailed))
+                }
+            }
+            task?.resume()
+        }
+                       }
