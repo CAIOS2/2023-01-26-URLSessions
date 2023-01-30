a6a80b4e0648d766f51319792508f6cda5c9413d
rpromas <rpromas@gmail.com>
2023-01-30 20:20:34 +0200

    added codingkeys for people

--- urlsessions/DataModels/People.swift
+++ urlsessions/DataModels/People.swift
@@ -9,4 +9,10 @@ import Foundation
 
 struct People: Codable {
     let name: String
+    let hair: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case hair = "hair_color"
+    }
 }
