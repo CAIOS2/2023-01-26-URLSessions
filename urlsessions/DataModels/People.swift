40bf8f11f62598e82e676ab67701baafe50c18a3
Povilas <povilas.jocius@outlook.com>
2023-01-30 19:48:58 +0200

    defined codingKeys eyeColor and hairColor

--- urlsessions/DataModels/People.swift
+++ urlsessions/DataModels/People.swift
@@ -9,6 +9,15 @@ import Foundation
 
 struct People: Codable {
     let name: String
-    let birthYear: String
-    let hairColor: String
+    let birth: String
+    let eyes: String
+    let hair: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case birth = "birth_year"
+        case eyes = "eye_color"
+        case hair = "hair_color"
+    }
 }
+
