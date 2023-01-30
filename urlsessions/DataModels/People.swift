2c8b0c743586862d749595e8f0830e9978d5c0ba
Rolandas-R <driveris@gmail.com>
2023-01-30 21:47:22 +0200

    Created custom decoder in People struct

--- urlsessions/DataModels/People.swift
+++ urlsessions/DataModels/People.swift
@@ -9,14 +9,26 @@ import Foundation
 
 struct People: Codable {
     let name: String
-    let birthYear: String
-    let hair: String
-    let eyes: String
-    
+    let hairs: [String]
+    let mass: Int
+    let height: Int
+
     enum CodingKeys: String, CodingKey {
         case name
-        case birthYear = "birth_year"
-        case hair = "hair_color"
-        case eyes = "eye_color"
+        case hairs = "hair_color"
+        case mass
+        case height
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        name = try values.decode(String.self, forKey: .name)
+        let hairsValue = try values.decode(String.self, forKey: .hairs)
+        hairs = hairsValue.split(separator: ";").map {String($0) }
+        let massValue = try values.decode(String.self, forKey: .mass)
+        mass = Int(massValue) ?? 0
+        let heightValue = try values.decode(String.self, forKey: .height)
+        height = Int(heightValue) ?? 0
+        
     }
 }
--- urlsessions/PeopleViewController.swift
+++ urlsessions/PeopleViewController.swift
@@ -67,17 +67,17 @@ extension PeopleViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         tableData?.count ?? 0
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "peopleCell", for: indexPath)
         guard let person = tableData?[indexPath.row] else {
-               return cell
-           }
-           let name = person.name
-           let birthYear = person.birthYear
-           let hair = person.hair
-           let eyes = person.eyes
-           cell.textLabel?.text = "\(name) | \(birthYear) | \(hair) | \(eyes)"
+            return cell
+        }
+        let name = person.name
+        let hairs = person.hairs
+        let mass = person.mass
+        let height = person.height
+        cell.textLabel?.text = "\(name) | \(mass) | \(hairs) | \(height)"
         return cell
     }
 }
